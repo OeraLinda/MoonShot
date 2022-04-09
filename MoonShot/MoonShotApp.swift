614a6e60fd09bdf3d51c387c9ae7ab8f76aa9020
Yasin <yasinerdemli35@icloud.com>
2022-04-09 23:32:43 +0300

    Initial Commit

---
+++ MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by Yasin Erdemli on 9.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
